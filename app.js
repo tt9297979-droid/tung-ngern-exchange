26e4c631deaf0a2d3694c8221101bfe65c2ff0d2
tt9297979-droid <tt9297979@gmail.com>
2026-03-15 02:41:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -48,4 +48,18 @@ document.getElementById("result").innerText=data.status
 
 reader.readAsDataURL(file)
 
+}function uploadSlip(){
+
+let file=document.getElementById("slip").files[0]
+
+if(!file){
+
+alert("กรุณาอัพโหลดสลิป")
+
+return
+
+}
+
+document.getElementById("result").innerText="ส่งสลิปแล้ว รอตรวจสอบ"
+
 }
