783df2c652a9346eab690e6ca9c5b16c001a918f
tt9297979-droid <tt9297979@gmail.com>
2026-03-15 02:37:09 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+
+API_KEY: "YOUR_API_KEY",
+
+QR_API:
+"https://connect.slip2go.com/api/qr-payment/generate-qr-code",
+
+VERIFY_API:
+"https://connect.slip2go.com/api/verify-slip/qr-image/info"
+
+}
