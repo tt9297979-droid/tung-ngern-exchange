ade3f54935e42121cf114de5c8e6d8157d3ffcc1
tt9297979-droid <tt9297979@gmail.com>
2026-03-15 02:43:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,15 @@
-async function createQR(){
-
-let amount=document.getElementById("amount").value
-
-let res=await fetch(CONFIG.QR_API,{
-method:"POST",
-headers:{
-"Content-Type":"application/json",
-"x-api-key":CONFIG.API_KEY
-},
-body:JSON.stringify({
-amount:amount,
-promptpay:"0611037042"
-})
-})
-
-let data=await res.json()
-
-document.getElementById("qr").src=data.qrCode
-
-}
-
-
-async function verifySlip(){
-
-let file=document.getElementById("slip").files[0]
-
-let reader=new FileReader()
-
-reader.onload=async function(){
-
-let base64=reader.result
-
-let res=await fetch(CONFIG.VERIFY_API,{
-method:"POST",
-headers:{
-"Content-Type":"application/json",
-"x-api-key":CONFIG.API_KEY
-},
-body:JSON.stringify({image:base64})
-})
-
-let data=await res.json()
-
-document.getElementById("result").innerText=data.status
-
-}
-
-reader.readAsDataURL(file)
-
-}function uploadSlip(){
+function uploadSlip(){
 
 let file=document.getElementById("slip").files[0]
 
 if(!file){
 
 alert("กรุณาอัพโหลดสลิป")
-
 return
 
 }
 
-document.getElementById("result").innerText="ส่งสลิปแล้ว รอตรวจสอบ"
+document.getElementById("result").innerText =
+"ส่งสลิปแล้ว ระบบกำลังตรวจสอบ"
 
 }
